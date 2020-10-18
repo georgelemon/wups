3a8324e504bc4364e1b5b3707f8a21dcb17f008e
George Lemon <things@wemake.co.de>
2020-10-19 00:18:17 +0300

    Vanilla JS

--- src/Resources/js/wups.base.js
+++ src/Resources/js/wups.base.js
@@ -123,6 +123,22 @@ let ajaxEditors        = document.querySelector('.editor-link[data-ajax]');
     });
 
     /**
+     * Make the given frames-tab active
+     */
+    function setActiveFramesTab(tab) {
+        
+        $tab.addClass('frames-tab-active');
+
+        if ($tab.attr('id') == 'application-frames-tab') {
+            $frameContainer.addClass('frames-container-application');
+            $allFramesTab.removeClass('frames-tab-active');
+        } else {
+            $frameContainer.removeClass('frames-container-application');
+            $appFramesTab.removeClass('frames-tab-active');
+        }
+    }
+
+    /**
      * Clipboard Plugin
      */
     function fallbackMessage(action) {
@@ -152,80 +168,22 @@ let ajaxEditors        = document.querySelector('.editor-link[data-ajax]');
     clipboard.on('error', function(e) {
         showTooltip(e.trigger, fallbackMessage(e.action));
     });
-    
+
     document.querySelector('.clipboard').addEventListener('mouseleave', function(e) {
         e.currentTarget.classList.add('clipboard');
         e.currentTarget.removeAttribute('aria-label');
-    });    
+    });
 
+    /**
+     * Render late enough for highlightCurrentLine to be ready
+     */
+    renderCurrentCodeblock();
 
-Zepto(function($) {
-  // var $leftPanel      = $('.left-panel');
-  var $frameContainer = $('.frames-container');
-  var $appFramesTab   = $('#application-frames-tab');
-  var $allFramesTab   = $('#all-frames-tab');
-  var $container      = $('.details-container');
-  var $activeLine     = $frameContainer.find('.frame.active');
-  var $activeFrame    = $container.find('.frame-code.active');
-  var $ajaxEditors    = $('.editor-link[data-ajax]');
-
-  $(document).on('keydown', function(e) {
-    var applicationFrames = $frameContainer.hasClass('frames-container-application'),
-        frameClass = applicationFrames ? '.frame.frame-application' : '.frame';
-
-	  if(e.ctrlKey || e.which === 74  || e.which === 75) {
-		  // CTRL+Arrow-UP/k and Arrow-Down/j support:
-		  // 1) select the next/prev element
-		  // 2) make sure the newly selected element is within the view-scope
-		  // 3) focus the (right) container, so arrow-up/down (without ctrl) scroll the details
-		  if (e.which === 38 /* arrow up */ || e.which === 75 /* k */) {
-			  $activeLine.prev(frameClass).click();
-			  scrollIntoView($activeLine, leftPanel);
-			  $container.focus();
-			  e.preventDefault();
-		  } else if (e.which === 40 /* arrow down */ || e.which === 74 /* j */) {
-			  $activeLine.next(frameClass).click();
-			  scrollIntoView($activeLine, leftPanel);
-			  $container.focus();
-			  e.preventDefault();
-		  }
-	  } else if (e.which == 78 /* n */) {
-      if ($appFramesTab.length) {
-        setActiveFramesTab($('.frames-tab:not(.frames-tab-active)'));
-      }
-    }
-  });
-
-  // Render late enough for highlightCurrentLine to be ready
-  renderCurrentCodeblock();
-
-  // Avoid to quit the page with some protocol (e.g. IntelliJ Platform REST API)
-  $ajaxEditors.on('click', function(e){
-    e.preventDefault();
-    $.get(this.href);
-  });
-
-  // Symfony VarDumper: Close the by default expanded objects
-  $('.sf-dump-expanded')
-    .removeClass('sf-dump-expanded')
-    .addClass('sf-dump-compact');
-  $('.sf-dump-toggle span').html('&#9654;');
-
-  // Make the given frames-tab active
-  function setActiveFramesTab($tab) {
-    $tab.addClass('frames-tab-active');
-
-    if ($tab.attr('id') == 'application-frames-tab') {
-      $frameContainer.addClass('frames-container-application');
-      $allFramesTab.removeClass('frames-tab-active');
-    } else {
-      $frameContainer.removeClass('frames-container-application');
-      $appFramesTab.removeClass('frames-tab-active');
-    }
-  }
-
-  $('a.frames-tab').on('click', function(e) {
-    e.preventDefault();
-    setActiveFramesTab($(this));
-  });
-});
+    /**
+     * Avoid to quit the page with some protocol (e.g. IntelliJ Platform REST API)
+     * @todo
+     */
+    // ajaxEditors.addEventListener('click', function(event){
+        // event.preventDefault();
+        // $.get(this.href);
+    // });
